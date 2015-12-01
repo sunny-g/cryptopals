2057dae32ff41bd2cdecbc3afc80448ba2b68adb
sunny-g <sunny.gonna@gmail.com>
2015-11-30 17:41:15 -0800

    initial commit

---
+++ tests.js
@@ -0,0 +1,73 @@
+describe('set 1', function() {
+  describe('1.1 hex to base64', function() {
+    it('should convert hex to base64', function() {
+
+    });
+  });
+
+  describe('1.2 fixed XOR', function() {
+    it('should test something', function() {
+
+    });
+  });
+
+  describe('1.3 single-byte XOR cipher', function() {
+    it('should test something', function() {
+
+    });
+  });
+
+  describe('1.4 detect single-char XOR', function() {
+    it('should test something', function() {
+
+    });
+  });
+
+  describe('1.5 implementing repeating-key XOR', function() {
+    it('should test something', function() {
+
+    });
+  });
+
+  describe('1.6 break repeating-key XOR', function() {
+    it('should test something', function() {
+
+    });
+  });
+
+  describe('1.7 AES in ECB mode', function() {
+    it('should test something', function() {
+
+    });
+  });
+
+  describe('1.8 detect AES in ECB mode', function() {
+    it('should test something', function() {
+
+    });
+  });
+});
+
+describe('set 2', function() {
+
+});
+
+describe('set 3', function() {
+
+});
+
+describe('set 4', function() {
+
+});
+
+describe('set 5', function() {
+
+});
+
+describe('set 6', function() {
+
+});
+
+describe('set 7', function() {
+
+});
